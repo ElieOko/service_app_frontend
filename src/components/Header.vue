db5cab786370c80eb3851d96ef3ae7345b2f901c
Elie Oko <elieoko100@gmail.com>
2023-12-19 08:06:43 +0100

    fix header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import { clearUser } from '@/stores/user';
+import { clearUser, getUser } from '@/stores/user';
 import { useSidebar } from '@/utils/constant/fun';
 import { ref } from 'vue'
+import { useRouter } from 'vue-router';
 
 const dropdownOpen = ref(false)
 const { isOpen } = useSidebar()
+const router = useRouter();
+    const user = getUser();
+    const logout =()=>{
+      clearUser()
+      router.push("/login");
+    }
 </script>
 
 <template>
@@ -38,22 +45,9 @@ const { isOpen } = useSidebar()
     </div>
 
     <div class="flex items-center">
-      <button class="flex mx-4 text-gray-600 focus:outline-none">
-        <svg
-          class="w-6 h-6"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </button>
+      <span class="flex mx-4 text-gray-600 focus:outline-none">
+        {{ user?.username }}
+      </span>
 
       <div class="relative">
         <button
@@ -66,7 +60,6 @@ const { isOpen } = useSidebar()
             alt="Your avatar"
           >
         </button>
-
         <div
           v-show="dropdownOpen"
           class="fixed inset-0 z-10 w-full h-full"
@@ -85,9 +78,7 @@ const { isOpen } = useSidebar()
             v-show="dropdownOpen"
             class="absolute right-0 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl"
           >
-           
-           
-            <button @click="clearUser()"
+            <button @click="logout()"
               class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white"
             >
               Deconnexion
