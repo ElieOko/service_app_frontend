08784b456c82e76be498b9416b68b7333bc21a04
Elie Oko <elieoko100@gmail.com>
2024-01-01 13:59:39 +0100

    build electront test

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow, BrowserWindowConstructorOptions, ipcMain, screen } from "electron";
+import path from "path";
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('./dist/index.html') // this needs to be changed to './dist/index.html' for the my-vue-app
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
